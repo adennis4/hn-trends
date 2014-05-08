9135c0e7887beabeba6c23f35fff92e8b92c97d3
Hashrocket Workstation <adennis4@gmail.com>
2014-05-08 11:12:57 -0500

    Move over to Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HNTrends::Application.config.secret_key_base = 'e26874d1a2888d7c7a720212f540a2b529485fd12e809a3ffc2d15fe1bf7dfa8cc7a3023972c89271d41ba9abbd0f12340d28a4566083c9424d665d57001ca5f'
